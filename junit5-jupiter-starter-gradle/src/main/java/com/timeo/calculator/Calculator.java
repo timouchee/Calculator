f5d8b19df6fa680529ce98f65bcbf967b46f682c
timeo <timeo971@gmail.com>
2024-09-24 17:44:39 +0200

    exo 9

--- junit5-jupiter-starter-gradle/src/main/java/com/timeo/calculator/Calculator.java
+++ junit5-jupiter-starter-gradle/src/main/java/com/timeo/calculator/Calculator.java
@@ -3,12 +3,12 @@ package com.timeo.calculator;
 public class Calculator {
 
 
-    public int add(int opG, int opD)
+    public static int add(int opG, int opD)
     {
        return opG+opD;
     }
 
-    public int divide(int opG, int opD)
+    public static int divide(int opG, int opD)
     {
         return opG/opD;
     }
--- junit5-jupiter-starter-gradle/src/test/java/com/timeo/calculator/CalculatorTest.java
+++ junit5-jupiter-starter-gradle/src/test/java/com/timeo/calculator/CalculatorTest.java
@@ -1,6 +1,9 @@
 package com.timeo.calculator;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +18,17 @@ class CalculatorTest {
         calculator = new Calculator();
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",  // opG = 0, opD = 1, res = 1
+            "1, 2, 3",  // opG = 1, opD = 2, res = 3
+            "-2, 2, 0", // opG = -2, opD = 2, res = 0
+            "0, 0, 0",  // opG = 0, opD = 0, res = 0
+            "-1, -2, -3"// opG = -1, opD = -2, res = -3
+    })
+    void devrait_faire_des_add_en_list(int opG, int opD, int res) {
+        assertEquals(res, Calculator.add(opG, opD));
+    }
 
     @Test
     void devrait_faire_laddition_2_et_5()
@@ -27,8 +41,6 @@ class CalculatorTest {
     {
         assertEquals(5,calculator.divide(10,2));
     }
-
-
 
     @AfterEach
     void enUp()
